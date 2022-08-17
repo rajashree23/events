3f9a4da7958ee342cfcacc912994567ea7691169
Rajashree Parhi <rajashree.parhi@factset.com>
2022-08-17 23:01:08 +0530

    added removeEventListener.js

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,6 +1,12 @@
 const btn = document.querySelector('button');
 
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', changeText);
+
+btn.removeEventListener('click', changeText);
+
+
+function changeText(){
+  console.log('hey')
   btn.textContent="Button clicked!!🌟";
-});
+}
